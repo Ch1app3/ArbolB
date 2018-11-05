a65c336fed92fda8c90533842d82842a059ab779
Ch1app3 <44758952+Ch1app3@users.noreply.github.com>
2018-11-04 22:28:30 -0300

    Add files via upload

---
+++ Nodo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolB
+{
+    class Nodo
+    {
+        private Int32 _Dato;
+
+        public Int32 Dato
+        {
+            get { return _Dato; }
+            set { _Dato = value; }
+        }
+        private Nodo _Padre;
+                    
+        public Nodo Padre
+        {
+            get { return _Padre; }
+            set { _Padre = value; }
+        }
+        private Nodo _hijoDer;
+
+        public Nodo hijoDer
+        {
+            get { return _hijoDer; }
+            set { _hijoDer = value; }
+        }
+        private Nodo _hijoIzq;
+
+        public Nodo hijoIzq
+        {
+            get { return _hijoIzq; }
+            set { _hijoIzq = value; }
+        }
+
+
+
+    }
+}

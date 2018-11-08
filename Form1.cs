3532ef5d4642ae7d86198c04d37060e73a420e1a
Ch1app3 <44758952+Ch1app3@users.noreply.github.com>
2018-11-08 20:56:04 -0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -71,5 +71,22 @@ namespace WindowsFormsApplication1
 
             }
         }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back)) && (e.KeyChar == (char)Keys.Subtract))
+            {
+                MessageBox.Show("Solo se permiten numeros", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+            
+        
+
+            
+
+        }
+
+        
     }
 }
